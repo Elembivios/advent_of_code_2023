879c664eaafb2be7c3780ffc6c60e58adbeccad5
Martin KOSCAK <martin.koscak@posta.si>
2023-12-04 14:33:10 +0100

    Added basic structure.

---
+++ src/day_01/mod.rs
@@ -0,0 +1,19 @@
+pub struct Trebuchet {
+
+}
+
+impl crate::Advent for Trebuchet {
+    fn new(data: &str) -> Self
+        where 
+            Self: Sized {
+        Self {}
+    }
+
+    fn part_01(&self) -> String {        
+        1.to_string()
+    }
+
+    fn part_02(&self) -> String {
+        2.to_string()
+    }
+}
---
+++ src/day_02/mod.rs
@@ -0,0 +1,19 @@
+pub struct CubeConundrum {
+
+}
+
+impl crate::Advent for CubeConundrum {
+    fn new(data: &str) -> Self
+        where 
+            Self: Sized {
+        Self { }
+    }
+
+    fn part_01(&self) -> String {
+        1.to_string()
+    }
+
+    fn part_02(&self) -> String {
+        2.to_string()
+    }
+}
